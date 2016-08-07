d26a0a2fc4993693fcd35d8d17165c247a625415
chaluemwutnoyunsan <chaluemwut@hotmail.com>
2016-08-07 18:12:14 +0700

    #

--- web/model/toyota_documents.py
+++ web/model/toyota_documents.py
@@ -1,12 +1,16 @@
 from mongoengine import *
 from config import Config
 from bson.objectid import ObjectId
-import datetime
+import datetime, os
 
-connect('toyoya', host=Config.db_host)
+connect('toyoya', host=os.environ['TOYOTA_DB_1_PORT_27017_TCP_ADDR'])
 
 class Problem(Document):
     problem_level = StringField()
     title = StringField()
     message = StringField()
     create_date = DateTimeField(default=datetime.datetime.now)
+
+print('***** test db')
+p = Problem.objects
+print(p)
